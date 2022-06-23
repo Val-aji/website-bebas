5465e137b0581618260d2e92e3fdd6bc45fa846d
Nouval <ajinopal80@gmail.com>
2022-06-23 23:52:16 +0700

    memperbaiki bug tampilan home

--- index.js
+++ index.js
@@ -4,15 +4,13 @@ let bgOpacityWeb = document.querySelector(".bgOpacityWeb");
 bgWeb.className = "bgWeb contaienr-fluid";
 bgOpacityWeb.className = "bgOpacityWeb d-flex flex-column justify-content-evenly";
 
-
 let judul = document.querySelector(".judul");
- judul.className = "judul d-flex flex-column align-items-center justify-content-center";
+
+judul.className = "judul d-flex flex-column align-items-center justify-content-center";
  
 let tentangWeb = document.querySelector(".tentangWeb");
 
 if(window.innerWidth < 768) {
-   
-    
     judul.children[0].className = "display-4 f-roboto ls-2 text-white ";
     judul.children[1].remove();
     bgOpacityWeb.children[1].className = "d-flex justify-content-center p-3";
@@ -28,18 +26,16 @@ if(window.innerWidth < 768) {
 } else {
     
     
-    
-    
     judul.children[0].className = "display-6 f-roboto ls-20 text-white ";
     judul.children[1].className = "lead text-white d-block fs-4";
     bgOpacityWeb.children[1].className = "d-flex justify-content-between p-3";
     for(let x of bgOpacityWeb.children[1].children) {
        x.className = "p-3 m-5 btn btn-info text-dark fw-bold f-average ls-2 rounded-5";
     }
-    tentangWeb.className = "tentangWeb container-fluid px-3 py-5 d-flex justify-content-between";
+    tentangWeb.className = "tentangWeb container-fluid px-3 py-5 d-flex justify-content-around";
     tentangWeb.children[0].className = "text-end";
-    for(let y of tentangWeb.childeren) {
-        y.style.width = 45%;
+    for(let y of tentangWeb.children) {
+        y.style.width = "45%"
     }
     
 }
